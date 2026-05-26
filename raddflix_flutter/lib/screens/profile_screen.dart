d138a7d5f0ec2fd514793da8f709893910dff830
raddclub <clubradd@gmail.com>
2026-05-26 10:07:06 -0700

    fix: add missing connectivity_plus import in profile_screen.dart

--- raddflix_flutter/lib/screens/profile_screen.dart
+++ raddflix_flutter/lib/screens/profile_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
+import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../core/constants.dart';
 import '../services/vault_service.dart';
